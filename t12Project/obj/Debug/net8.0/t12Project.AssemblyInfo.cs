d256d9b35a423aba5b203f9a63230390b5853d09
BrightonDube <BrightonDube@users.noreply.github.com>
2025-11-17 20:35:14 +0200

    initial commit

--- t12Project/obj/Debug/net8.0/t12Project.AssemblyInfo.cs
+++ t12Project/obj/Debug/net8.0/t12Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("t12Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97466fc2a5640bfb5940449a377c033f81c8c24f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa26b38c5e1be73861652d481dec369e1c62a6d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("t12Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("t12Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
