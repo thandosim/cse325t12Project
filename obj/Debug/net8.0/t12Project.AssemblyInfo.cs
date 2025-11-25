cbcea08850147c822701b640122e83fb9da7663b
BrightonDube <BrightonDube@users.noreply.github.com>
2025-11-25 15:11:17 +0200

    merge

--- obj/Debug/net8.0/t12Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/t12Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("t12Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ca00a64cb78994f24874751ed9eb427d9c64f45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85002ac02547aab9628d2099491cd0af5b3315e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("t12Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("t12Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
