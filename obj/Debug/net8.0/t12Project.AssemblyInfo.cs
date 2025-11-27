07918dd1da9ac9a8018d709319bd45ff28d2b915
thandosim <sim24001@byui.edu>
2025-11-27 20:09:34 +0200

    feat(dashboard): separate load table data from map pins in DriverDashboard
    - Kept loadItems as string-based rows for DashboardTable
    - Introduced loadPins as tuple-based coordinates + labels for MapView
    - Updated MapView to use loadPins instead of loadItems
    - Ensured driverLocation centers the map correctly
    - Note: map container renders but Leaflet map is not displaying yet (to be solved later)

--- obj/Debug/net8.0/t12Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/t12Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("t12Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61a0fb2b1c93343a3365caf233bcf19a9e90be43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2bf2de133f1d5a59d843afa1c62a5ab34105840")]
 [assembly: System.Reflection.AssemblyProductAttribute("t12Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("t12Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
